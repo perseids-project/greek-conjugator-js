aeb5f7d92488c07d7a37379019bca9c98c4c56df
zfletch <zfletch2@gmail.com>
2019-06-13 14:27:08 +0100

    Updates

---
+++ precache-manifest.0c49926125abb917bde36ed9d387c487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f1fca6bdeb33d1ef5687da17ca0d06",
+    "url": "/greek-conjugator/index.html"
+  },
+  {
+    "revision": "40d73a98f8934318f5dc",
+    "url": "/greek-conjugator/static/css/2.776713bd.chunk.css"
+  },
+  {
+    "revision": "738cf5491c7829682e1f",
+    "url": "/greek-conjugator/static/css/main.f4cbe5f1.chunk.css"
+  },
+  {
+    "revision": "40d73a98f8934318f5dc",
+    "url": "/greek-conjugator/static/js/2.4dc4d6e9.chunk.js"
+  },
+  {
+    "revision": "25377bf1f7af471e7710",
+    "url": "/greek-conjugator/static/js/3.d0deadde.chunk.js"
+  },
+  {
+    "revision": "738cf5491c7829682e1f",
+    "url": "/greek-conjugator/static/js/main.cbef0f95.chunk.js"
+  },
+  {
+    "revision": "40439e7093c8e0ca2cb5",
+    "url": "/greek-conjugator/static/js/runtime~main.f1b9d9c9.js"
+  }
+]);
