0988b9f7834410c2f2361b1c8f8caf748acc7e3c
zfletch <zfletch2@gmail.com>
2019-06-13 14:26:24 +0100

    Updates

---
+++ precache-manifest.d5348a203db42e6a812e3dcd1b47ed25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37f3b434df1e50eccbdfc0d38cbc8e5",
+    "url": "/greek-conjugator-js/index.html"
+  },
+  {
+    "revision": "40d73a98f8934318f5dc",
+    "url": "/greek-conjugator-js/static/css/2.776713bd.chunk.css"
+  },
+  {
+    "revision": "3dd31a63008a8a666778",
+    "url": "/greek-conjugator-js/static/css/main.f4cbe5f1.chunk.css"
+  },
+  {
+    "revision": "40d73a98f8934318f5dc",
+    "url": "/greek-conjugator-js/static/js/2.4dc4d6e9.chunk.js"
+  },
+  {
+    "revision": "25377bf1f7af471e7710",
+    "url": "/greek-conjugator-js/static/js/3.d0deadde.chunk.js"
+  },
+  {
+    "revision": "3dd31a63008a8a666778",
+    "url": "/greek-conjugator-js/static/js/main.54cdc196.chunk.js"
+  },
+  {
+    "revision": "0aedfc47f4ed67eb6446",
+    "url": "/greek-conjugator-js/static/js/runtime~main.d5ada04a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/greek-conjugator-js/precache-manifest.d03f71e343b6b931e8d9e70487029c7d.js"
+  "/greek-conjugator-js/precache-manifest.d5348a203db42e6a812e3dcd1b47ed25.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/greek-conjugator-js/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greek-conjugator-js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
