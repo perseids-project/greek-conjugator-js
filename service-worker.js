3629cad733c0753c8140abe97f18970dacc9e55e
zfletch <zfletch2@gmail.com>
2020-01-09 14:49:55 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/greek-conjugator/precache-manifest.fda12ac82f1ee8ba9416cd9a2edf1bee.js"
+  "/greek-conjugator/precache-manifest.db867b4b653b7b9b1bb5912e1e330b7b.js"
 );
 
 self.addEventListener('message', (event) => {
